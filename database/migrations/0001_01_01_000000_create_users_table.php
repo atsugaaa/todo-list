ec323d19b9f0c189b76a1dec649406a391980ae1
atsugaa <kurniaagusta50@gmail.com>
2025-02-10 00:54:09 +0700

    migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,19 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->primary('username');
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('todos', function (Blueprint $table) {
+            $table->string('todos_id');
+            $table->string('todo');
+            $table->boolean('is_completed')->default(false);
+            $table->primary('todos_id');
+            $table->string('user');
+            $table->foreign('user')->references('username')->on('users')->onDelete('cascade');
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -43,7 +43,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('todos');
         Schema::dropIfExists('sessions');
     }
 };
